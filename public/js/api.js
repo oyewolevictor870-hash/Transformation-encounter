1b4a23b0aeed09e318526af12655e4fbe2843a59
oyewolevictor870-hash <oyewolevictor870@gmail.com>
2026-05-02 00:02:12 +0100

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -5,7 +5,7 @@
 // ============================================
 
 const API = {
-    baseUrl: '/api',
+    baseUrl: 'https://transformation-encounter.production.up.railway.app/api',
 
     getToken() {
         return localStorage.getItem('te_token');
